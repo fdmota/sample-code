e617d331564af9543020b5ed585c1ac4d7fb75d9
Filipe Mota <f.mota@carepay.com>
2020-11-09 18:27:09 +0100

    JAVA

--- java/learn1/Animal.java
+++ java/learn1/Animal.java
@@ -1,7 +1,13 @@
 public class Animal {
 
+  static final String[] MY_STATE_VALUES = {"PENDING", "PROCESSING", "PROCESSED"};
+
   public static String iAmDog() {
     return "I am a dog";
+  }
+
+  public String getName() {
+    return "bob";
   }
 }
 
--- java/learn1/Hello.java
+++ java/learn1/Hello.java
@@ -18,6 +18,38 @@ class Hello {
       a.add(2);
       a.add(3);
       System.out.println(a);
+
+      // Arrays
+      int[] myInts = new int[7];
+      System.out.println(myInts.length);
+      Animal[] myAnimals = new Animal[7];
+      for(int i = 0; i < myAnimals.length; i++) {
+        myAnimals[i] = new Animal();
+      }
+      // Foreach
+      for(Animal animal : myAnimals) {
+        System.out.println(animal.getName());
+      }
+      System.out.println(myAnimals.getClass());
+
+      // Array defined with {}
+      final Animal[] myAnimals2 = {myAnimals[0], null, myAnimals[6]};
+      System.out.println(myAnimals2.length);
+      
+      // final static enum
+      // LoggingLevel state = LoggingLevel.PENDING;
+      for(LoggingLevel state : LoggingLevel.values()) {
+        System.out.println(state.code());
+        if (state.equals(LoggingLevel.PENDING)) {
+          System.out.println("It is pending");
+        }
+        // switch(state) {
+
+        // }
+      }
+
+    // https://www.youtube.com/watch?v=grEKMHGYyns
+    // 2:41:38
   }
 
   public static void addExclamationPoint(String s) {
